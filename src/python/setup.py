668322e1e0a3c5ceb0d8c40ec1c73031d99ca13f
Tom Oinn <tomoinn@crypticsquid.com>
2015-08-24 23:34:19 +0100

    Added doc build / upload

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,6 +1,12 @@
 __author__ = 'tom'
 from setuptools import setup
 
+# Makes use of the sphinx and sphinx-pypi-upload packages. To build for local development
+# use 'python setup.py develop'. To upload a version to pypi use 'python setup.py clean sdist upload'.
+# To build docs use 'python setup.py build_sphinx' and to upload docs to pythonhosted.org use
+# 'python setup.py upload_sphinx'. Both uploads require 'python setup.py register' to be run, and will
+# only work for Tom as they need the pypi account credentials.
+
 setup(
     name='triangula',
     version='0.1',
