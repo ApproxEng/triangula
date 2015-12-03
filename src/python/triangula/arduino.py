9eeee1fbd744132aecc9023ca6e767ec6b4c6b92
Tom Oinn <tomoinn@crypticsquid.com>
2015-12-03 01:21:29 +0000

    Updated support for the i2c devices

--- src/python/triangula/arduino.py
+++ src/python/triangula/arduino.py
@@ -5,20 +5,34 @@ from time import sleep
 import smbus
 
 ARDUINO_ADDRESS = 0x70
-DEVICE_MOTORS_SET = 0x20
-DEVICE_LIGHTS_SET = 0x21
-DEVICE_ENCODERS_READ = 0x22
 
 I2C_RETRIES = 4
 I2C_DELAY = 0.01
 
 
 def float_to_byte(f):
+    """
+    Map a float from -1.0 to 1.0 onto the range 0-255 and return as a byte-compatible number.
+    Used when sending bytes for e.g. the speed controllers.
+
+    :param f:
+        A float, between -1.0 and 1.0. Values outside this range will not behave sensibly.
+    :return:
+        An integer where 128 is equivalent to 0, 0 to -1.0 and 255 to +1.0
+    """
     i = int((f + 1.0) * 128.0)
     return check_byte(i)
 
 
 def check_byte(b):
+    """
+    Clamp the supplied value to an integer between 0 and 255 inclusive
+
+    :param b:
+        A number
+    :return:
+        Integer representation of the number, limited to be between 0 and 255
+    """
     i = int(b)
     if i < 0:
         i = 0
@@ -39,35 +53,73 @@ class Arduino:
     elsewhere, and based on the code at http://dsscircuits.com/articles/arduino-i2c-slave-guide.
     """
 
-    def __init__(self, i2c_delay=I2C_DELAY, max_retries=I2C_RETRIES, bus_id=1):
-        self.bus = smbus.SMBus(1)
-        self.i2c_delay = i2c_delay
-        self.max_retries = max_retries
+    DEVICE_MOTORS_SET = 0x20
+    DEVICE_LIGHTS_SET = 0x21
+    DEVICE_ENCODERS_READ = 0x22
+
+    def __init__(self,
+                 arduino_address=ARDUINO_ADDRESS,
+                 i2c_delay=I2C_DELAY,
+                 max_retries=I2C_RETRIES,
+                 bus_id=1):
+        """
+        Create a new client, uses the I2C bus to communicate with the arduino.
+
+        :param arduino_address:
+            7-bit address of the arduino on the I2C bus
+        :param i2c_delay:
+            Delay in seconds, used when we either retry transactions or inbetween immediately
+            consecutive read and write operations. This attempts to mitigate problems we've had
+            where transactions have failed, probably because the arduino is still handling the
+            previous read / write mode and gets confused.
+        :param max_retries:
+            The maximum number of retries to attempt when an IOError occurs during a transaction.
+        :param bus_id:
+            The bus ID used when initialising the smbus library. For the Pi2 this is 1, others may
+            be on bus 0.
+        """
+        self._bus = smbus.SMBus(bus_id)
+        self._i2c_delay = i2c_delay
+        self._max_retries = max_retries
+        self._address = arduino_address
 
     def _send(self, register, data):
-        retries_left = self.max_retries
+        retries_left = self._max_retries
         while retries_left > 0:
             try:
-                self.bus.write_i2c_block_data(ARDUINO_ADDRESS, register, data)
+                self._bus.write_i2c_block_data(self._address, register, data)
                 return
             except IOError:
-                sleep(self.i2c_delay)
+                sleep(self._i2c_delay)
                 retries_left -= 1
                 pass
         raise IOError("Retries exceeded sending data to arduino.")
 
     def _read(self, register, bytes_to_read):
-        retries_left = self.max_retries
+        """
+        Work around issues we seem to have with bulk transfers by transferring a byte at
+        a time after manually triggering the population of the read buffer on the arduino.
+
+        :param register:
+            The register to write a 0 to in order to start the read transaction. The slave
+            device should respond to this by populating the export array, this is then read
+            a byte at a time.
+        :param bytes_to_read:
+            The number of times the read_byte method needs to be called
+        :return:
+            A byte array, length equal to bytes_to_read, containing data read from the arduino.
+        """
+        retries_left = self._max_retries
         while retries_left > 0:
             try:
                 # Prod the appropriate control register
-                self.bus.write_byte_data(ARDUINO_ADDRESS, register, 0)
+                self._bus.write_byte_data(self._address, register, 0)
                 # Delay for an arbitrary amount of time
-                sleep(self.i2c_delay)
+                sleep(self._i2c_delay)
                 # Call read_byte repeatedly to assemble our output data
-                return [self.bus.read_byte(ARDUINO_ADDRESS) for _ in xrange(bytes_to_read)]
+                return [self._bus.read_byte(self._address) for _ in xrange(bytes_to_read)]
             except IOError:
-                sleep(self.i2c_delay)
+                sleep(self._i2c_delay)
                 retries_left -= 1
         raise IOError("Retries exceeded when fetching data from arduino.")
 
@@ -86,24 +138,31 @@ class Arduino:
                         float_to_byte(b),
                         float_to_byte(c)]
 
-        self._send(DEVICE_MOTORS_SET, motor_values)
+        self._send(Arduino.DEVICE_MOTORS_SET, motor_values)
 
     def set_lights(self, hue, saturation, value):
         """
-        Set the neopixel lights to a constant value
-        :param hue: 0-255 hue
-        :param saturation: 0-255 saturation
-        :param value: 0-255 value
+        Set all the neopixel lights to a constant value
+
+        :param hue:
+            0-255 hue
+        :param saturation:
+            0-255 saturation
+        :param value:
+            0-255 value
         """
         light_values = [check_byte(hue), check_byte(saturation), check_byte(value)]
-        self._send(DEVICE_LIGHTS_SET, light_values)
+        self._send(Arduino.DEVICE_LIGHTS_SET, light_values)
 
     def get_encoder_values(self):
         """
-        Read data from the encoders, returning as a triple of what would be a uint16 if we had such things.
-        :return: Triple of encoder values for each wheel.
+        Read data from the encoders, returning as a triple of what would be a uint16
+        if we had such things.
+
+        :return:
+            Triple of encoder values for each wheel.
         """
-        encoder_data = self._read(DEVICE_ENCODERS_READ, 6)
+        encoder_data = self._read(Arduino.DEVICE_ENCODERS_READ, 6)
         print encoder_data
         return [encoder_data[0] * 256 + encoder_data[1],
                 encoder_data[2] * 256 + encoder_data[3],
